d01ded4146633f4d42959a3c3e0e07a4403e1048
Guilherme Gabriel <guilherme.gabriel.532@gmail.com>
2022-05-31 02:58:56 -0300

    add api

---
+++ Api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+
+app.get("/github/repos/:account", async (req, res) => {
+    const account = req.params.account;
+    console.log(`GET - /github/repos/${account}`);
+    const lang = req.query.lang || "C#";
+    const limit = req.query.limit || 5;
+    const order = req.query.order || "ASC"
+
+    const ghResponse = await fetch(`https://api.github.com/users/${account}/repos`).then(res=>res.json());
+
+    res.json(
+        Array.from(ghResponse)
+        .filter( repo => repo.language === lang )
+        .sort((a,b) => compareDateStrings(a.created_at,b.created_at) * ((order === "DESC") ? -1 : 1))
+        .slice(0,limit)
+        .map(repo => ({
+            avatar: repo.owner.avatar_url,
+            name: repo.full_name,
+            description: repo.description ?? "",
+            created_at: new Date(repo.created_at).toLocaleDateString(),
+            lang: repo.language
+        }))
+    );
+});
+
+function compareDateStrings(dateA, dateB){
+    return new Date(dateA).getTime() - new Date(dateB).getTime()
+}
+
+app.listen(3001, ()=>{
+    console.log("listening to port 3001");
+})
